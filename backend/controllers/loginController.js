43cb8e2bb7c774a8800313f19579d95dd61196ca
Sithila69 <kms123sithila@gmail.com>
2025-03-11 17:00:46 +0530

    Update loginController.js

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -48,7 +48,7 @@ export const loginUser = async (req, res) => {
     res.cookie("authToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Match CSRF settings
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
